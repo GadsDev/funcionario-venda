2a09796a82dc2088d1d1b439ad9595b7b2e8216d
GadsDev <gusttavo212@gmail.com>
2019-07-25 17:43:20 -0300

    Create MCS

--- vendas-api/src/app.module.ts
+++ vendas-api/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose'
+import { SellersModule } from './sellers/sellers.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://gadsdev:a123456@ds153314.mlab.com:53314/vendas'
-    , { useNewUrlParser : true})
+    , { useNewUrlParser : true}),
+    SellersModule
   ],
   controllers: [AppController],
   providers: [AppService],
