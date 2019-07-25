8f746400921c897e55a0f5177945b796f545e773
GadsDev <gusttavo212@gmail.com>
2019-07-25 17:24:36 -0300

    Connect Mongo

--- vendas-api/src/app.module.ts
+++ vendas-api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://gadsdev:a123456@ds153314.mlab.com:53314/vendas'
+    , { useNewUrlParser : true})
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
