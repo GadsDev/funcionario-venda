b3b1bae45c36ea8e49371f7328d8d2b32a104e9e
GadsDev <gusttavo212@gmail.com>
2019-07-26 15:51:54 -0300

    Import Schema on module

--- vendas-api/src/sellers/sellers.module.ts
+++ vendas-api/src/sellers/sellers.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SellersController } from './sellers.controller';
 import { SellersService } from './sellers.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SellerSchema } from './seller.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Seller', schema: SellerSchema}])
+  ],
   controllers: [SellersController],
   providers: [SellersService]
 })
